501efbb84458ff7f02e0ed5b369b19c8d5d69301
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-17 23:42:19 -0300

    V13: finalize tested price pipeline version

--- v122.js
+++ v122.js
@@ -2,7 +2,7 @@
 (function(){
   'use strict';
 
-  const APP_VERSION='V13.0-test';
+  const APP_VERSION='V13.0';
   const $v=(s,r=document)=>r.querySelector(s);
   const sleep=ms=>new Promise(r=>setTimeout(r,ms));
   const finishSelections=new Map();
