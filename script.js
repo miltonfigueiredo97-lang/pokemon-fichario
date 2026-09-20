f24af835be51c81640db494593ce336f9e184f79
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-20 13:18:49 -0300

    chore: bump binder fixes to 14.33

--- script.js
+++ script.js
@@ -470,7 +470,7 @@ async function registerPWA(){
       reloading=true;
       location.reload();
     });
-    const reg=await navigator.serviceWorker.register("/sw.js?v=14.32",{updateViaCache:"none"});
+    const reg=await navigator.serviceWorker.register("/sw.js?v=14.33",{updateViaCache:"none"});
     await reg.update();
     if(reg.waiting)reg.waiting.postMessage({type:"SKIP_WAITING"});
     reg.addEventListener("updatefound",()=>{
