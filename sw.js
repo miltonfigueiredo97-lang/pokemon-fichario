1521de0deb4f24c78184e52f0947ce4b03b7f62d
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-21 21:17:57 -0300

    chore: bump PWA cache to V14.43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BUILD_VERSION = '14.42';
-const CACHE_NAME = 'pokemon-binder-v14-42';
+const BUILD_VERSION = '14.43';
+const CACHE_NAME = 'pokemon-binder-v14-43';
 const APP_SHELL = [
   '/',
   '/index.html',
