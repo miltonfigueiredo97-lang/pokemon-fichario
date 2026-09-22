b0d8b9ba4d919e2ab4086e6daba8d86d91e6b63f
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-21 22:14:54 -0300

    chore: bump cache to V14.47

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BUILD_VERSION = '14.46';
-const CACHE_NAME = 'pokemon-binder-v14-46';
+const BUILD_VERSION = '14.47';
+const CACHE_NAME = 'pokemon-binder-v14-47';
 const APP_SHELL = [
   '/',
   '/index.html',
