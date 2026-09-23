9ebe80b43279c554d7f97ada96554d9440826235
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-23 16:43:34 -0300

    Register V14.75 service worker

--- script.js
+++ script.js
@@ -1044,7 +1044,7 @@ async function registerPWA(){
       reloading=true;
       location.reload();
     });
-    const reg=await navigator.serviceWorker.register("/sw.js?v=14.74",{updateViaCache:"none"});
+    const reg=await navigator.serviceWorker.register("/sw.js?v=14.75",{updateViaCache:"none"});
     await reg.update();
     if(reg.waiting)reg.waiting.postMessage({type:"SKIP_WAITING"});
     reg.addEventListener("updatefound",()=>{
