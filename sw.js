eb43523161a7528541044c858686c5a9e11773da
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-23 15:38:44 -0300

    Refresh PWA cache for V14.69

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const BUILD_VERSION = '14.68';
-const CACHE_NAME = 'pokemon-binder-v14-68';
+const BUILD_VERSION = '14.69';
+const CACHE_NAME = 'pokemon-binder-v14-69';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=14.68',
-  '/v8.css?v=14.68',
-  '/v11fix.css?v=14.68',
-  '/v12.css?v=14.68',
-  '/v122.css?v=14.68',
-  '/v140.css?v=14.68',
-  '/script.js?v=14.68',
-  '/v8.js?v=14.68',
-  '/v11fix.js?v=14.68',
-  '/v12.js?v=14.68',
-  '/v122.js?v=14.68',
-  '/v140.js?v=14.68',
+  '/style.css?v=14.69',
+  '/v8.css?v=14.69',
+  '/v11fix.css?v=14.69',
+  '/v12.css?v=14.69',
+  '/v122.css?v=14.69',
+  '/v140.css?v=14.69',
+  '/script.js?v=14.69',
+  '/v8.js?v=14.69',
+  '/v11fix.js?v=14.69',
+  '/v12.js?v=14.69',
+  '/v122.js?v=14.69',
+  '/v140.js?v=14.69',
   '/manifest.webmanifest',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg'
