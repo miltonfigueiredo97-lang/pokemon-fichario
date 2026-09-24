c058760a5698346471ff35562307141bc94f2b0b
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-24 16:00:52 -0300

    V14.95 bump app version

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const BUILD_VERSION = '14.94';
-const CACHE_NAME = 'pokemon-binder-v14-94';
+const BUILD_VERSION = '14.95';
+const CACHE_NAME = 'pokemon-binder-v14-95';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=14.94',
-  '/v8.css?v=14.94',
-  '/v11fix.css?v=14.94',
-  '/v12.css?v=14.94',
-  '/v122.css?v=14.94',
-  '/v140.css?v=14.94',
-  '/script.js?v=14.94',
-  '/v8.js?v=14.94',
-  '/v11fix.js?v=14.94',
-  '/v12.js?v=14.94',
-  '/v122.js?v=14.94',
-  '/v140.js?v=14.94',
+  '/style.css?v=14.95',
+  '/v8.css?v=14.95',
+  '/v11fix.css?v=14.95',
+  '/v12.css?v=14.95',
+  '/v122.css?v=14.95',
+  '/v140.css?v=14.95',
+  '/script.js?v=14.95',
+  '/v8.js?v=14.95',
+  '/v11fix.js?v=14.95',
+  '/v12.js?v=14.95',
+  '/v122.js?v=14.95',
+  '/v140.js?v=14.95',
   '/manifest.webmanifest',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg'
