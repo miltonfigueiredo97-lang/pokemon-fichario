866e7952f96349074c1a633b45f2bf23d5bdebda
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-25 16:25:25 -0300

    Bump service worker cache to v16.13

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const BUILD_VERSION = '16.12';
-const CACHE_NAME = 'pokemon-binder-v16-12';
+const BUILD_VERSION = '16.13';
+const CACHE_NAME = 'pokemon-binder-v16-13';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=16.12',
-  '/v8.css?v=16.12',
-  '/v11fix.css?v=16.12',
-  '/v12.css?v=16.12',
-  '/v122.css?v=16.12',
-  '/v140.css?v=16.12',
-  '/script.js?v=16.12',
-  '/v8.js?v=16.12',
-  '/v11fix.js?v=16.12',
-  '/v12.js?v=16.12',
-  '/v122.js?v=16.12',
-  '/v140.js?v=16.12',
+  '/style.css?v=16.13',
+  '/v8.css?v=16.13',
+  '/v11fix.css?v=16.13',
+  '/v12.css?v=16.13',
+  '/v122.css?v=16.13',
+  '/v140.css?v=16.13',
+  '/script.js?v=16.13',
+  '/v8.js?v=16.13',
+  '/v11fix.js?v=16.13',
+  '/v12.js?v=16.13',
+  '/v122.js?v=16.13',
+  '/v140.js?v=16.13',
   '/manifest.webmanifest',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg'
