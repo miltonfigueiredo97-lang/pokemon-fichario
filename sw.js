84ff758fbaefd5d9e80da13e875760eb06e3104c
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-25 07:27:04 -0300

    V16.08 bump direct MYP progress build

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const BUILD_VERSION = '16.07';
-const CACHE_NAME = 'pokemon-binder-v16-07';
+const BUILD_VERSION = '16.08';
+const CACHE_NAME = 'pokemon-binder-v16-08';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=16.07',
-  '/v8.css?v=16.07',
-  '/v11fix.css?v=16.07',
-  '/v12.css?v=16.07',
-  '/v122.css?v=16.07',
-  '/v140.css?v=16.07',
-  '/script.js?v=16.07',
-  '/v8.js?v=16.07',
-  '/v11fix.js?v=16.07',
-  '/v12.js?v=16.07',
-  '/v122.js?v=16.07',
-  '/v140.js?v=16.07',
+  '/style.css?v=16.08',
+  '/v8.css?v=16.08',
+  '/v11fix.css?v=16.08',
+  '/v12.css?v=16.08',
+  '/v122.css?v=16.08',
+  '/v140.css?v=16.08',
+  '/script.js?v=16.08',
+  '/v8.js?v=16.08',
+  '/v11fix.js?v=16.08',
+  '/v12.js?v=16.08',
+  '/v122.js?v=16.08',
+  '/v140.js?v=16.08',
   '/manifest.webmanifest',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg'
