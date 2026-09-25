793973dee04b7b7637a438b5da5b44e7695c4a14
miltonfigueiredo97-lang <miltonfigueiredo97@gmail.com>
2026-09-24 23:40:58 -0300

    V16.01 bump Japanese search build

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-const BUILD_VERSION = '16.00';
-const CACHE_NAME = 'pokemon-binder-v16-00';
+const BUILD_VERSION = '16.01';
+const CACHE_NAME = 'pokemon-binder-v16-01';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=16.00',
-  '/v8.css?v=16.00',
-  '/v11fix.css?v=16.00',
-  '/v12.css?v=16.00',
-  '/v122.css?v=16.00',
-  '/v140.css?v=16.00',
-  '/script.js?v=16.00',
-  '/v8.js?v=16.00',
-  '/v11fix.js?v=16.00',
-  '/v12.js?v=16.00',
-  '/v122.js?v=16.00',
-  '/v140.js?v=16.00',
+  '/style.css?v=16.01',
+  '/v8.css?v=16.01',
+  '/v11fix.css?v=16.01',
+  '/v12.css?v=16.01',
+  '/v122.css?v=16.01',
+  '/v140.css?v=16.01',
+  '/script.js?v=16.01',
+  '/v8.js?v=16.01',
+  '/v11fix.js?v=16.01',
+  '/v12.js?v=16.01',
+  '/v122.js?v=16.01',
+  '/v140.js?v=16.01',
   '/manifest.webmanifest',
   '/icons/icon-192.svg',
   '/icons/icon-512.svg'
